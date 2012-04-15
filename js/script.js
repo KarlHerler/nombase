35892198e71ca3c72fd839616aef817bec50e238
Karl Herler <hi@karlherler.com>
2012-04-15 19:33:13 +0300

    Using script.js as a dummy interface to persistant storage.

--- js/script.js
+++ js/script.js
@@ -4,5 +4,11 @@
 
 
 
+var recipes = [{
+	"title": 		"Pizza",
+	"ingredients": "1tsp salt\n1dl flour",
+	"instructions": "1. Make a delicious pizza.\n2. ???\n3. Profit.",
+	"tags": ["Pizza", "Meat", "Baking"]
+}]
 
-
+var tags = ["Pizza", "Meat", "Baking", "Chicken", "Cow"]
