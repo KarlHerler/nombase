597e9d004260e073bb42cc4da3f843208d79f720
Karl Herler <hi@karlherler.com>
2012-04-16 00:17:29 +0300

    Added more debug data.

--- js/script.js
+++ js/script.js
@@ -4,15 +4,24 @@
 
 
 
-var recipes = [{
-	"title": 		"Pizza",
-	"ingredients": "1tsp salt\n1dl flour",
-	"instructions": "1. Make a delicious pizza.\n2. ???\n3. Profit.",
-	"tags": ["Pizza", "Meat", "Baking"]
-}]
+var recipes = [
+	{
+		"title": 		"Pizza",
+		"ingredients": 	"1tsp salt<br>1dl flour",
+		"instructions": "1. Mix.<br>2. ???<br>3. Profit.",
+		"tags": 		["Pizza", "Meat", "Baking"]
+	},
+	{
+		"title": 		"Pie",
+		"ingredients": 	"2tsp salt<br>5dl flour",
+		"instructions": "1. Mix.<br>2. ???<br>3. Profit.",
+		"tags": 		["Pie", "Meat", "Baking"]
+	}
+]
 
 var tags = [
 			{"name": "Pizza"}, 
+			{"name": "Pie"},
 			{"name": "Meat"}, 
 			{"name":"Baking"}, 
 			{"name":"Chicken"}, 
--- src/main.coffee
+++ src/main.coffee
@@ -1,5 +1,10 @@
 window.recipes = new Recipes(recipes)
 window.tags = new Tags(tags)
 window.tagbar = new TagBarView(tags)
+window.recipeview = new RecipeView(recipes.models[0])
 
 tagbar.render()
+
+
+recipeview.render()
+$(".main").html(recipeview.$el)
