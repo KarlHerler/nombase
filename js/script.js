cc65dffcaeb9691f8dba10095c90085431a58899
Karl Herler <hi@karlherler.com>
2012-04-16 14:04:59 +0300

    Added more debug data.

--- js/script.js
+++ js/script.js
@@ -16,6 +16,12 @@ var recipes = [
 		"ingredients": 	"2tsp salt<br>5dl flour",
 		"instructions": "1. Mix.<br>2. ???<br>3. Profit.",
 		"tags": 		["Pie", "Meat", "Baking"]
+	},
+	{
+		"title": 		"Chocolate",
+		"ingredients": 	"2tsp sugar<br>5dl flour<br>1block of chocolate",
+		"instructions": "1. Throw away the sugar.<br>2. Throw away the flour.<br>3. Enjoy the chocolate.<br>4. ???<br>5. Profit.",
+		"tags": 		["Pie", "Meat", "Baking"]
 	}
 ]
 
