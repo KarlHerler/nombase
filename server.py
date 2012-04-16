f338ddffc96327fe09a69f3d439dc2d2c45bcc26
Karl Herler <hi@karlherler.com>
2012-04-16 18:52:38 +0300

    Updated with heroku needed additions.

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
@@ -12,4 +14,6 @@ def recipe(r):
 
 
 if __name__ == "__main__":
-	app.run()
+	 # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
