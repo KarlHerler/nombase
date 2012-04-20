0372fa78e6d945e4c482c33a7869a1c6652c3909
Karl Herler <hi@karlherler.com>
2012-04-20 14:09:42 +0300

    Added a scrolling position fix effect to the controlbar.

--- static/js/script.js
+++ static/js/script.js
@@ -2,34 +2,12 @@
 
 */
 
-
-
-var recipes = [
-	{
-		"title": 		"Pizza",
-		"ingredients": 	"1tsp salt<br>1dl flour",
-		"instructions": "1. Mix.<br>2. ???<br>3. Profit.",
-		"tags": 		["Pizza", "Meat", "Baking"]
-	},
-	{
-		"title": 		"Pie",
-		"ingredients": 	"2tsp salt<br>5dl flour",
-		"instructions": "1. Mix.<br>2. ???<br>3. Profit.",
-		"tags": 		["Pie", "Meat", "Baking"]
-	},
-	{
-		"title": 		"Chocolate",
-		"ingredients": 	"2tsp sugar<br>5dl flour<br>1block of chocolate",
-		"instructions": "1. Throw away the sugar.<br>2. Throw away the flour.<br>3. Enjoy the chocolate.<br>4. ???<br>5. Profit.",
-		"tags": 		["Pie", "Meat", "Baking"]
+$(window).scroll(function() {
+	if ($(window).scrollTop()>160) {
+		$(".controls").css("position", "fixed");
+		$(".controls").css("top", 0)
+	} else {
+		$(".controls").css("position", "absolute");
+		$(".controls").css("top", 162)
 	}
-]
-
-var tags = [
-			{"name": "Pizza"}, 
-			{"name": "Pie"},
-			{"name": "Meat"}, 
-			{"name":"Baking"}, 
-			{"name":"Chicken"}, 
-			{"name":"Cow"}
-		   ]
+})
