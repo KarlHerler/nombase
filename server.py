30ef8dbc479ac51bca8315a0d43a997e8a140f79
Karl Herler <hi@karlherler.com>
2012-05-17 18:28:48 +0300

    Turned off debugmode in the server for release push.

--- server.py
+++ server.py
@@ -54,5 +54,5 @@ def edit_recipe(r):
 if __name__ == "__main__":
 	 # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.debug = True
+    #app.debug = True
     app.run(host='0.0.0.0', port=port)
